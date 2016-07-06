73910c68e7dfc16a8cfec349b35a26df871e759c
Neo Chen <superanan0401@yahoo.com.tw>
2016-07-06 09:52:49 +0800

    Test2

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(136, 223);
+            this.button1.Location = new System.Drawing.Point(136, 192);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
