ed0793c8deb74b214fdfbe2ff9ef558f415ab6d5
Neo Chen <superanan0401@yahoo.com.tw>
2016-07-06 09:50:52 +0800

    Test1

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -34,11 +34,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(136, 117);
+            this.button1.Location = new System.Drawing.Point(136, 223);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "button2";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
